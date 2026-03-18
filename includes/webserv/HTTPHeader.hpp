6b06e898431665def26d5a38aaa6c931a7ba85cf
pieric-ux <demont.pieric@protonmail.com>
2026-03-18 21:12:07 +0100

    feat: Implement HTTP Headers and HeaderRegistry

---
+++ includes/webserv/HTTPHeader.hpp
@@ -0,0 +1,43 @@
+// TODO: don't forget header
+
+#ifndef WEBSERV_HTTPHEADERS_HPP
+#define WEBSERV_HTTPHEADERS_HPP
+
+/**
+* @file HTTPHeaders.hpp
+* @brief [TODO:description]
+*/
+
+#include <string>
+
+namespace http
+{
+namespace HTTPheaders
+{
+
+class HTTPHeader
+{
+	public:
+		HTTPHeader();
+		HTTPHeader(const std::string name, const std::string description);
+		HTTPHeader(const std::string name, const std::string value, const std::string description);
+		~HTTPHeader();
+
+		HTTPHeader(const HTTPHeader &rhs);
+		HTTPHeader &operator=(const HTTPHeader &rhs);
+
+		std::string getName() const;
+		std::string getValue() const;
+		void 		setValue(const std::string value);
+		std::string getDescription() const;
+
+	private:
+		std::string _name;
+		std::string _value;
+		std::string _description;
+};
+
+} // !HTTPheaders
+} // !http
+
+#endif
