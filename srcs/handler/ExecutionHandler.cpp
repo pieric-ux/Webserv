0a8d1c354eb6b2c92ff93ef9484d51cf236fa40d
AzraeDomilite <benedict.lucken74@gmail.com>
2026-04-13 14:25:34 +0200

    FIX : infinty loop on put content lenght 0, flag exec complete never set

--- srcs/handler/ExecutionHandler.cpp
+++ srcs/handler/ExecutionHandler.cpp
@@ -603,7 +603,11 @@ void ExecutionHandler::writeChunk(const handler::RequestHandler &requestHandler)
 	std::size_t		remaining = std::stoul(it->second.front().getValue()) - offset;
 	std::size_t		available = buf.size() > offset ? buf.size() - offset : 0;
 	if (available == 0)
+	{
+		setFlags(getFlags() | E_EXEC_COMPLETE);
+		_fd.reset();
 		return ;
+	}
 	std::size_t		toWrite = remaining < config::DefaultConfig::BUFFER_SIZE ? remaining : config::DefaultConfig::BUFFER_SIZE;
 	toWrite = toWrite < available ? toWrite : available;
 	ssize_t			wr;
