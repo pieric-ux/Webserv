1e56e2962dfd8373d7d682c415cb91af1c879782
AzraeDomilite <benedict.lucken74@gmail.com>
2026-04-13 16:16:07 +0200

    DOC : update error pages uml class diagram

--- srcs/config/ErrorPage.cpp
+++ srcs/config/ErrorPage.cpp
@@ -19,6 +19,59 @@ namespace config
  */
 static std::string	formatStatusCodesInfo(const t_StatusCodes &codes);
 
+
+/**
+ * @brief [TODO:description]
+ */
+ErrorPage::ErrorPage() : _codes(), _path()
+{
+	_logger = log42::manager::Manager::getInstance().getLogger("webserv.config.ErrorPage");
+	_logger->setLevel(log42::logRecord::DEBUG);
+	INFO(_logger, "ErrorPage instance created with default constructor");
+}
+
+/**
+ * @brief [TODO:description]
+ *
+ * @param codes [TODO:parameter]
+ * @param path [TODO:parameter]
+ */
+ErrorPage::ErrorPage(const t_StatusCodes &codes, const std::string &path)
+:	_codes(codes),
+_path(path)
+{
+	_logger = log42::manager::Manager::getInstance().getLogger("webserv.config.ErrorPage");
+	_logger->setLevel(log42::logRecord::DEBUG);
+	INFO(_logger, "ErrorPage instance created with status codes " + formatStatusCodesInfo(codes) + " and path: " + path);
+}
+
+/**
+ * @brief [TODO:description]
+ */
+ ErrorPage::~ErrorPage() {}
+ 
+
+/**
+ * @brief [TODO:description]
+ *
+ * @param rhs [TODO:parameter]
+ */
+ErrorPage::ErrorPage(const ErrorPage &rhs)
+:	_logger(rhs._logger),
+_codes(rhs._codes),
+_path(rhs._path)
+{}
+
+/**
+ * @brief [TODO:description]
+ *
+ * @return [TODO:return]
+ */
+t_Logger	ErrorPage::getLogger()
+{
+	return log42::manager::Manager::getInstance().getLogger("webserv.config.ErrorPage");
+}
+
 /**
  * @brief [TODO:description]
  *
@@ -38,57 +91,6 @@ const std::string	&ErrorPage::getPath() const
 {
 	return _path;
 }
-
-/**
- * @brief [TODO:description]
- */
-ErrorPage::ErrorPage() : _codes(), _path()
-{
-	_logger = log42::manager::Manager::getInstance().getLogger("webserv.config.ErrorPage");
-	_logger->setLevel(log42::logRecord::DEBUG);
-	INFO(_logger, "ErrorPage instance created with default constructor");
-}
-
-/**
- * @brief [TODO:description]
- *
- * @param codes [TODO:parameter]
- * @param path [TODO:parameter]
- */
-ErrorPage::ErrorPage(const t_StatusCodes &codes, const std::string &path)
-	:	_codes(codes),
-		_path(path)
-{
-	_logger = log42::manager::Manager::getInstance().getLogger("webserv.config.ErrorPage");
-	_logger->setLevel(log42::logRecord::DEBUG);
-	INFO(_logger, "ErrorPage instance created with status codes " + formatStatusCodesInfo(codes) + " and path: " + path);
-}
-
-/**
- * @brief [TODO:description]
- */
-ErrorPage::~ErrorPage() {}
-
-/**
- * @brief [TODO:description]
- *
- * @return [TODO:return]
- */
-t_Logger	ErrorPage::getLogger()
-{
-	return log42::manager::Manager::getInstance().getLogger("webserv.config.ErrorPage");
-}
-
-/**
- * @brief [TODO:description]
- *
- * @param rhs [TODO:parameter]
- */
-ErrorPage::ErrorPage(const ErrorPage &rhs)
-	:	_logger(rhs._logger),
-		_codes(rhs._codes),
-		_path(rhs._path)
-{}
 
 /**
  * @brief [TODO:description]
